fe2885faa0a0e8ff45b3081a5d793b93767901d7
Bumjin Im <bi1@rice.edu>
2021-03-17 10:33:55 -0500

    Add missing dependencies

--- script/dep.sh
+++ script/dep.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-sudo apt install build-essential git cmake libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf libdwarf-dev libdw-dev libaio-dev libpcre3-dev libtool uuid-dev libblkid-dev apache2-utils automake autoconf libtool-bin wget
+sudo apt install build-essential git cmake libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf libdwarf-dev libdw-dev libaio-dev libpcre3-dev libtool uuid-dev libblkid-dev apache2-utils automake autoconf libtool-bin wget dwarves
